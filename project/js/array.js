548bd3d16c3e8576c0327388bebdd37d4fcf3066
Nathan Chen <113192742+seribical@users.noreply.github.com>
2023-11-13 13:06:29 -0500

    save o werk

---
+++ project/js/array.js
@@ -0,0 +1,16 @@
+const array = [
+    {
+        character:g,
+        afflatus: g,
+        damage: g,
+        rarity: g,
+        incantations:[
+            spell1='g',
+            spell2='g',
+            ult='g',
+        ],
+    },
+    {
+
+    },
+]
