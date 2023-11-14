bb6ef1a2c283e56fc5af8084123ef16debb564bd
Nathan Chen <113192742+seribical@users.noreply.github.com>
2023-11-14 13:11:43 -0500

    asdasdsad

--- project/js/array.js
+++ project/js/array.js
@@ -1,16 +1,74 @@
 const array = [
     {
-        character:g,
-        afflatus: g,
-        damage: g,
-        rarity: g,
+        character:"Melania",
+        afflatus:"Beast",
+        damage:"Mental",
+        rarity: 6,
         incantations:[
             spell1='g',
             spell2='g',
             ult='g',
         ],
+        splash:""
     },
     {
-
+        character:"Lilya",
+        afflatus:"Star",
+        damage:"Reality",
+        rarity: 6,
+        incantations:[
+            spell1='g',
+            spell2='g',
+            ult='g',
+        ],
+        splash:""
+    },
+    {
+        character:"A Knight",
+        afflatus:"Spirit",
+        damage:"Reality",
+        rarity: 6,
+        incantations:[
+            spell1='g',
+            spell2='g',
+            ult='g',
+        ],
+        splash:""
+    },
+    {
+        character:"Sotheby",
+        afflatus:"Plant",
+        damage:"Reality",
+        rarity: 6,
+        incantations:[
+            spell1='g',
+            spell2='g',
+            ult='g',
+        ],
+        splash:""
+    },
+    {
+        character:"Regulus",
+        afflatus:"Star",
+        damage:"Mental",
+        rarity: 6,
+        incantations:[
+            spell1='g',
+            spell2='g',
+            ult='g',
+        ],
+        splash:""
+    },
+    {
+        character:"Sonetto",
+        afflatus:"Mineral",
+        damage:"Physical",
+        rarity: 5,
+        incantations:[
+            spell1='g',
+            spell2='g',
+            ult='g',
+        ],
+        splash:""
     },
 ]
